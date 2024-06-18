388bd6892603d6c9b317b4390234005d8a6ad60f
Esteban <e.leivam@uniandes.edu.co>
2024-06-17 19:19:13 -0500

    First Iteration Erspa Without Tests

--- src/erspa_star.jl
+++ src/erspa_star.jl
@@ -36,16 +36,7 @@ function euclidean_distance!(erspa::ERSPAstar)
 end
 
 function F(erspa::ERSPAstar, path::Vector{Int})
-    mean = 0.0
-    variance_term = 0.0
-    covariance_term = 0.0
-    for i in 1:length(path) - 1
-        mean += erspa.G.nodes[path[i]].links[path[i+1]].mean
-        variance_term += erspa.G.nodes[path[i]].links[path[i+1]].variance
-        if length(path)>1 && i < length(path) - 1
-            covariance_term += erspa.covariance_dict[(path[i], path[i+1], path[i+1], path[i+2])]
-        end
-    end
+    mean, variance_term, covariance_term = get_path_distribution(erspa.G, path, erspa.covariance_dict)
     dist = Normal(mean, √(variance_term + covariance_term))
     f = quantile(dist, erspa.α) + erspa.node_distances[path[end]]
     return f
@@ -97,12 +88,72 @@ function check_NC_link(erspa::ERSPAstar, link::Tuple{Int, Int})
     return true
 end
 
-function check_MB_dominance()
+function check_MB_dominance(erspa::ERSPAstar, path::Vector{Int})
+    link = (path[end-1], path[end])
+    v = 1
+    P = erspa.non_dominated_paths[link]
+    while v <= length(P)
+        mean_u, variance_term_u, covariance_term_u = get_path_distribution(erspa.G, path, erspa.covariance_dict)
+        mean_v, variance_term_v, covariance_term_v = get_path_distribution(erspa.G, P[collect(keys(P))[v]], erspa.covariance_dict)
+        if mean_u >= mean_v
+            dist_u = Normal(mean_u, √(variance_term_u + covariance_term_u))
+            dist_v = Normal(mean_v, √(variance_term_v + covariance_term_v))
+            if quantile(dist_u, erspa.α) > quantile(dist_v, erspa.α)
+                return true
+            end
+        else
+            break
+        end 
+        v += 1
+    end  
     return false
 end
 
-function check_dominance()
-    return false
+function check_dominance(erspa::ERSPAstar, path::Vector{Int})
+     link = (path[end-1], path[end])
+     if check_PC_link(erspa, link)
+         β = erspa.α
+     elseif check_NC_link(erspa, link) || check_MC_link(erspa, link)
+         if erspa.α > 0.5
+             β = 0.999
+         elseif erspa.α == 0.5
+             β = 0.5
+         else
+             β = 0.001
+         end
+     end
+
+     P_d = Set{Vector{Int}}()
+     v = 1
+     mean_u, variance_term_u, covariance_term_u = get_path_distribution(erspa.G, path, erspa.covariance_dict)
+     dist_u = Normal(mean_u, √(variance_term_u + covariance_term_u))
+     P = erspa.non_dominated_paths[link]
+     while v <= length(P)
+        mean_v, variance_term_v, covariance_term_v = get_path_distribution(erspa.G, P[collect(keys(P))[v]], erspa.covariance_dict)
+        if mean_u >= mean_v
+            dist_v = Normal(mean_v, √(variance_term_v + covariance_term_v))
+            if quantile(dist_u, β) > quantile(dist_v, β)
+                return P_d
+            end
+            v += 1
+        else
+            break
+        end
+    end
+    enqueue!(erspa.non_dominated_paths[link], path, mean_u)
+    v += 1
+    while v <= length(P)
+        mean_v, variance_term_v, covariance_term_v = get_path_distribution(erspa.G, P[collect(keys(P))[v]], erspa.covariance_dict)
+        dist_v = Normal(mean_v, √(variance_term_v + covariance_term_v))
+        if quantile(dist_u, β) < quantile(dist_v, β)
+            push!(P_d, P[collect(keys(P))[v]])
+            delete!(P, P[collect(keys(P))[v]])
+            v += 1
+        else
+            break
+        end
+    end 
+    return P_d
 end
 
 function initialization(erspa::ERSPAstar)
@@ -124,6 +175,40 @@ function path_selection(erspa::ERSPAstar)
     return true, path
 end
 
-function path_extension(erspa::ERSPAstar, path::Vector{Int})
-    return false
+function path_extension(erspa::ERSPAstar, path::Vector{Int}, link::Tuple{Int, Int})
+    if check_MC_link(erspa, link) 
+        MB = check_MB_dominance(erspa, path)
+    end
+    for (reachable_node, link) in erspa.G.nodes[link[2]].links
+        if check_MC_link(erspa, (link[2],reachable_node))
+            if MB && erspa.covariance_dict[link[1], link[2], link[2], reachable_node] >= 0
+                continue
+            else
+                new_path = copy(path)
+                push!(new_path, reachable_node)
+                f = F(erspa, new_path)
+                P_d = check_dominance(erspa, new_path)
+                if !isempty(P_d)
+                    enqueue!(SE, new_path, f)
+                    for p in P_d
+                        delete!(SE, p)
+                    end
+                end
+            end
+        end
+    end
+end
+
+function run_erspa(erspa::ERSPAstar)
+    initialization(erspa)
+    while true
+        p_s = path_selection(erspa)
+        if ps[1] == false && !isempty(ps[2])
+            erspa.optimal_path = ps[2]
+        elseif ps[1] == true
+            path_extension(erspa, ps[2], (ps[2][end-1], ps[2][end]))
+        else
+            println("No path found")
+        end
+    end
 end
